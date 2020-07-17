90fd3be29711a3544fc3baa51b8dbe6c89d79190
Joe Graf <geekydevjoe@gmail.com>
2020-07-17 09:01:57 -0700

    initial build commit

---
+++ src/build/precache-manifest.41667f860d4dcf5fc4780436eef2d7e5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4bb7b8ecc4d812362a090f49650ff698",
+    "url": "/index.html"
+  },
+  {
+    "revision": "bf6229b84597ef3e3d1e",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "d709eb8aa65ada021f5c",
+    "url": "/static/js/2.0503e762.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.0503e762.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf6229b84597ef3e3d1e",
+    "url": "/static/js/main.12a40756.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
